26679cb1134887122544bd81702f5121b83fc4ec
Souzatitan <asouzanderson@gmail.com>
2025-01-20 17:35:12 -0300

    responsive

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
-const hamburguerMenu = document.getElementById ('hamburguer-menu');
-const navMenu = document.getElementById ('nav-menu');
+const hamburguerMenu = document.getElementById('hamburguer-menu');
+const navMenu = document.getElementById('nav-menu');
 
-hamburguerMenu.addEventListener('click', () => {
+hamburguerMenu.addEventListener('click', (e) => {
+    e.stopPropagation();
     navMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (!hamburguerMenu.contains(e.target) && !navMenu.contains(e.target)) {
+        navMenu.classList.remove('active');
+    }
+
 });
